b831a18185e1e4d83662a04fc89273aef62d41eb
isabellecoxx <160355381+isabellecoxx@users.noreply.github.com>
2025-06-04 14:43:45 -0500

    setup database connection

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+    include("include/init.php");
+
+    echo "
+
+    <!DOCTYPE html>
+
+    <head>
+        <meta charset='utf-8' name='viewport' content='width=device-width'>
+        <link rel='stylesheet' href='mainPageStyle.css'>
+    </head>
+
+    <h1 class='titleText'>Testing Summer Project Webpage</h1>
+
+    <body style='background-color:pink;'>
+    ";
+
+    // Retrieve users from database and iterate through rows to print values
+
+    $users = getUsers();
+
+    foreach($users as $user){
+
+        $id = $user['userId'];
+        $name = $user['userName'];
+
+        echo "
+            <br>
+            <div class='classParagraph'>
+                ID: $id
+            </div>
+            <br>
+            <div class='classParagraph'>
+                Name: $name
+            </div>
+            <br>
+    ";
+    }
+
+    // I will add things like form functionality or other tables if needed :)
+?>
