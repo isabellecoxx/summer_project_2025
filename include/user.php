858aae5878633b9152031238da5eb9b44dfc4a10
Isabelle Cox <isabelle.cox@slu.edu>
2025-06-05 09:21:26 -0500

    updated based on feedback

---
+++ include/user.php
@@ -0,0 +1,24 @@
+<?php
+
+    // Functions that use queries to access information from the user table
+    function getUsers(){
+
+        $users = dbQuery("
+            SELECT *
+            FROM user
+        ")->fetchAll();
+
+        return $users;
+    }
+
+    function getUser($userId){
+
+        $user = dbQuery("
+            SELECT *
+            FROM projects
+            WHERE projectId = $userId;
+        ")->fetch();
+
+        return $user;
+
+    }
