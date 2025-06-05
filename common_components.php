5d3919789b19dbc23d4e3b7595dd43b32a40ffad
isabellecoxx <160355381+isabellecoxx@users.noreply.github.com>
2025-06-05 15:15:40 -0500

    update common_components.php

--- common_components.php
+++ common_components.php
@@ -6,7 +6,7 @@
 			    <html>
 			        <head>
 			            <title>".$pageTitle."</title>
-			            <link rel='stylesheet' type='text/css' href='/style.css'
+			            <link rel='stylesheet' href='mainPageStyle.css'>
 			        </head>
 			        <body>		            
 			";
