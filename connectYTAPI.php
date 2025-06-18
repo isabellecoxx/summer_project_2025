635c18c9ee0671b8e0514809b4beac1c8d32584d
Isabelle Cox <isabelle.cox@slu.edu>
2025-06-18 14:20:29 -0500

    ensuring remote changes match github

--- connectYTAPI.php
+++ connectYTAPI.php
@@ -67,9 +67,4 @@
 
         echo "<br><iframe width='560' height='315' src='https://www.youtube.com/embed/{$videoId}' frameborder='0' allowfullscreen></iframe>";
             
-    else {
-
-        echo "video not found";
-
-    }
 }
