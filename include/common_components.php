30028410559c57f1eece4ed9032329fb82f2958c
isabellecoxx <160355381+isabellecoxx@users.noreply.github.com>
2025-06-20 07:18:17 -0500

    add common_components.php

---
+++ include/common_components.php
@@ -0,0 +1,20 @@
+<?php
+
+    // Functions to echo out common components for every html page!!
+	function echoHeader($pageTitle){ 
+			echo "
+			    <html>
+			        <head>
+			            <title>".$pageTitle."</title>
+						<link rel='stylesheet' href='mainPageStyle.css'>
+			        </head>
+			        <body>		            
+			";
+	}
+
+    function echoFooter(){
+	    echo "
+			    </body>
+		    </html>
+	    ";
+    }
