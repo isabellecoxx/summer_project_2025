0435a50f4b38f76029ec15145b49cb6b8e110a28
isabellecoxx <160355381+isabellecoxx@users.noreply.github.com>
2025-07-14 10:13:31 -0500

    update init.php

--- include/init.php
+++ include/init.php
@@ -5,16 +5,10 @@
         session_start();
     }
 
-    //session_start();
-
     include('connect.php');
 		
     // This should happen right after connect.php (config)
     // so other functions have access to the database
-    // include('db_query.php');
-    // include('common_components.php');
-    // include('helper_functions.php');
-    // include('user.php');
 
     require_once('db_query.php');
     require_once('common_components.php');
